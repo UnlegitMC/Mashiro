eeaf0f27946504f19a657205b5af74686f0114df
liuli <liulihaocai@outlook.com>
2021-06-09 22:41:51 +0800

    framework

---
+++ src/main/java/me/liuli/mashiro/Mashiro.kt
@@ -0,0 +1,12 @@
+package me.liuli.mashiro
+
+object Mashiro {
+    @JvmStatic
+    val name="Mashiro"
+
+    @JvmStatic
+    val version="1.0.0"
+
+    @JvmStatic
+    val author="Liulihaocai"
+}
---
+++ src/main/java/me/liuli/mashiro/inject/mixins/client/MixinMinecraft.java
@@ -0,0 +1,17 @@
+package me.liuli.mashiro.inject.mixins.client;
+
+import me.liuli.mashiro.Mashiro;
+import net.minecraft.client.Minecraft;
+import org.lwjgl.opengl.Display;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Minecraft.class)
+public class MixinMinecraft {
+    @Inject(method = "createDisplay", at = @At(value = "INVOKE", target = "Lorg/lwjgl/opengl/Display;setTitle(Ljava/lang/String;)V", shift = At.Shift.AFTER))
+    private void createDisplay(CallbackInfo callbackInfo) {
+        Display.setTitle(Mashiro.getName()+" v"+Mashiro.getVersion()+" by "+Mashiro.getAuthor());
+    }
+}
