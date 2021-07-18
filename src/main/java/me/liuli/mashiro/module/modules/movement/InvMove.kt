d2bfa15c7c323424d53806fad0c4bccb1382af7d
func16 <51287366+func16@users.noreply.github.com>
2021-07-18 10:08:21 +0800

    Create InvMove.kt

---
+++ src/main/java/me/liuli/mashiro/module/modules/movement/InvMove.kt
@@ -0,0 +1,26 @@
+package me.liuli.mashiro.module.modules.movement
+
+import me.liuli.mashiro.event.EventMethod
+import me.liuli.mashiro.event.UpdateEvent
+import me.liuli.mashiro.module.Module
+import me.liuli.mashiro.module.ModuleCategory
+import me.liuli.mashiro.module.value.BoolValue
+import net.minecraft.client.gui.GuiChat
+import net.minecraft.client.gui.GuiIngameMenu
+import net.minecraft.client.gui.inventory.GuiContainer
+import net.minecraft.client.settings.GameSettings
+
+class InvMove : Module("InvMove","Allows you to walk while opening the inventory.", ModuleCategory.MOVEMENT) {
+    
+    @EventMethod;
+    fun onUpdate(event: UpdateEvent){
+         if(mc.currentScreen !is GuiChat && mc.currentScreen !is GuiIngameMenu && (!noDetectableValue.get() || mc.currentScreen !is GuiContainer)) {
+            mc.gameSettings.keyBindForward.pressed = GameSettings.isKeyDown(mc.gameSettings.keyBindForward);
+            mc.gameSettings.keyBindBack.pressed = GameSettings.isKeyDown(mc.gameSettings.keyBindBack);
+            mc.gameSettings.keyBindRight.pressed = GameSettings.isKeyDown(mc.gameSettings.keyBindRight);
+            mc.gameSettings.keyBindLeft.pressed = GameSettings.isKeyDown(mc.gameSettings.keyBindLeft);
+            mc.gameSettings.keyBindJump.pressed = GameSettings.isKeyDown(mc.gameSettings.keyBindJump);
+            mc.gameSettings.keyBindSprint.pressed = GameSettings.isKeyDown(mc.gameSettings.keyBindSprint);
+        }
+    }
+}
