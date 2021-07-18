cb0358b752e7a1cf4826dd0c1cca8aff797650d0
func16 <51287366+func16@users.noreply.github.com>
2021-07-18 10:55:06 +0800

    Create AntiAFK.kt

---
+++ src/main/java/me/liuli/mashiro/module/modules/misc/AntiAFK.kt
@@ -0,0 +1,19 @@
+package me.liuli.mashiro.module.modules.movement
+
+import me.liuli.mashiro.event.EventMethod
+import me.liuli.mashiro.event.UpdateEvent
+import me.liuli.mashiro.module.Module
+import me.liuli.mashiro.module.ModuleCategory
+
+
+class AntiAFK : Module("AntiAFK","Prevent being kicked out in AFK", ModuleCategory.MISC) {
+    
+    private var playerRotationYaw=0;
+  
+    @EventMethod
+    fun onUpdate(event: UpdateEvent){
+        mc.gameSettings.keyBindForward.pressed=true;
+        playerRotationYaw+=2;
+        mc.thePlayer.rotationYaw=playerRotationYaw%360
+    }
+}
