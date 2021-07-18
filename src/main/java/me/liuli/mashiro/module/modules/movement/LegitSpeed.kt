4c0df3ff03a766cb3df9347da210b7e7941ce69b
func16 <51287366+func16@users.noreply.github.com>
2021-07-18 09:42:05 +0800

    Create LegitSpeed.kt

---
+++ src/main/java/me/liuli/mashiro/module/modules/movement/LegitSpeed.kt
@@ -0,0 +1,27 @@
+package me.liuli.mashiro.module.modules.movement
+
+import me.liuli.mashiro.event.EventMethod
+import me.liuli.mashiro.event.UpdateEvent
+import me.liuli.mashiro.module.Module
+import me.liuli.mashiro.module.ModuleCategory
+import me.liuli.mashiro.module.value.BoolValue
+import me.liuli.mashiro.util.move.MovementUtils
+import net.minecraft.potion.Potion
+
+class LegitSpeed : Module("LegitSpeed","Automatically make you fast", ModuleCategory.MOVEMENT) {
+    
+    @EventMethod
+    fun onUpdate(event: UpdateEvent){
+        // Force sprint
+        if(!mc.thePlayer.isPotionActive(Potion.blindness) && mc.thePlayer.foodStats.foodLevel > 6.0f && !mc.thePlayer.isSneaking()){
+            if(mc.thePlayer.movementInput.moveForward==1.0){// It is FLOAT? Replace to 1.0f
+                mc.thePlayer.isSprinting=true;
+            }
+        }
+        
+        if(mc.thePlayer.onGround && !mc.thePlayer.isInWater() && !mc.thePlayer.isInLava()){
+            mc.thePlayer.jump();
+        }
+        
+    }
+}
